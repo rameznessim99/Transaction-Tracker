368fd95426913a8e205319a02004d0e01e23d5c7
Ramez Nessim <rameznessim99@gmail.com>
2022-07-29 23:08:01 -0400

    Addded all the new swift views

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView {
+                VStack(alignment: .leading, spacing: 24) {
+                    // MARK: Title
+                    Text("Overview")
+                        .font(.title2)
+                        .bold()
+                    
+                    // MARK: Transaction List
+                    RecentTransactionList()
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+            }
+            .background(Color.background)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    // MARK: Notification Icon
+                    Image(systemName: "bell.badge")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(Color.icon, .primary)
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+    
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+        .environmentObject(transactionListVM)
     }
 }
---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Ramez Nessim on 7/29/22.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
+
+extension DateFormatter {
+    static let allNumericUSA: DateFormatter = {
+        print("Initializing DateFormatter")
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        
+        return formatter
+    }()
+}
+
+extension String {
+    func dateParsed() -> Date {
+        guard let parsedDate = DateFormatter.allNumericUSA.date(from: self) else { return Date() }
+        
+        return parsedDate
+    }
+}
